7c2ddf2245be24a2cc27689cfd6e99ddab8fc123
msjian <msjian@talend.com>
2019-09-18 17:05:49 +0800

    fix(TDQ-16616): fix migration support context for report/connection well

--- main/plugins/org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/migration/impl/UpgradePasswordEncryptionAlg4DQItemTask.java
+++ main/plugins/org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/migration/impl/UpgradePasswordEncryptionAlg4DQItemTask.java
@@ -15,10 +15,22 @@ package org.talend.dataprofiler.core.migration.impl;
 import java.util.Date;
 import java.util.List;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.talend.commons.utils.PasswordEncryptUtil;
 import org.talend.core.model.metadata.builder.connection.DatabaseConnection;
+import org.talend.core.model.properties.Property;
+import org.talend.core.repository.model.ProxyRepositoryFactory;
 import org.talend.cwm.helper.TaggedValueHelper;
 import org.talend.dataprofiler.core.migration.AbstractWorksapceUpdateTask;
 import org.talend.dataquality.reports.TdReport;
+import org.talend.designer.core.model.utils.emf.talendfile.ContextParameterType;
+import org.talend.designer.core.model.utils.emf.talendfile.ContextType;
+import org.talend.designer.core.model.utils.emf.talendfile.util.TalendFileSwitch;
+import org.talend.dq.helper.PropertyHelper;
+import org.talend.dq.helper.resourcehelper.ContextResourceFileHelper;
 import org.talend.dq.helper.resourcehelper.PrvResourceFileHelper;
 import org.talend.dq.helper.resourcehelper.RepResourceFileHelper;
 import org.talend.dq.writer.impl.ElementWriterFactory;
@@ -40,20 +52,45 @@ public class UpgradePasswordEncryptionAlg4DQItemTask extends AbstractWorksapceUp
         return MigrationTaskType.FILE;
     }
 
+    TalendFileSwitch contextSwitch = new TalendFileSwitch() {
+
+        @Override
+        public ContextType caseContextType(ContextType object) {
+            return object;
+        };
+    };
+
     @Override
     protected boolean doExecute() throws Exception {
-        // for report datamart part, consider the password
-        List<? extends ModelElement> allElement = RepResourceFileHelper.getInstance().getAllElement();
-        for (ModelElement me : allElement) {
-            if (me instanceof TdReport) {
-                TdReport report = (TdReport) me;
-                TaggedValue oldPassword = TaggedValueHelper
-                        .getTaggedValue(TaggedValueHelper.REP_DBINFO_PASSWORD, report.getTaggedValue());
-                String oldPass = oldPassword.getValue();
-                if (oldPass != null) {
-                    String newPassword = PasswordMigrationUtil.getEncryptPasswordIfNeed(oldPass);
-                    TaggedValueHelper.setTaggedValue(report, TaggedValueHelper.REP_DBINFO_PASSWORD, newPassword); // after
-                    ElementWriterFactory.getInstance().createReportWriter().save(me);
+        // for context file, consider the password
+        List<IFile> contextList = ContextResourceFileHelper.getInstance().getAllContexts();
+        for (IFile contextFile : contextList) {
+            boolean modify = false;
+            ContextType type = null;
+            Resource fileResource = ContextResourceFileHelper.getInstance().getFileResource(contextFile);
+            EList<EObject> contents = fileResource.getContents();
+            for (EObject object : contents) {
+                Object doSwitch = contextSwitch.doSwitch(object);
+                type = doSwitch != null ? (ContextType) doSwitch : null;
+            }
+            if (type == null) {
+                continue;
+            }
+            // ContextType type = (ContextType) ContextResourceFileHelper.getInstance().getModelElement(contextFile);
+            List<ContextParameterType> paramTypes = type.getContextParameter();
+            if (paramTypes != null) {
+                for (ContextParameterType param : paramTypes) {
+                    String value = param.getValue();
+                    if (value != null && PasswordEncryptUtil.isPasswordType(param.getType())) {
+                        param.setRawValue(PasswordMigrationUtil.getEncryptPasswordIfNeed(value));
+                        modify = true;
+                    }
+                }
+            }
+            if (modify) {
+                Property property = PropertyHelper.getProperty(contextFile);
+                if (property != null) {
+                    ProxyRepositoryFactory.getInstance().save(property.getItem(), true);
                 }
             }
         }
@@ -63,13 +100,54 @@ public class UpgradePasswordEncryptionAlg4DQItemTask extends AbstractWorksapceUp
         for (ModelElement me : allConnectionElement) {
             if (me instanceof DatabaseConnection) {
                 DatabaseConnection dbConnection = (DatabaseConnection) me;
+                // for context mode database connection, keep it
+                if (dbConnection.isContextMode()) {
+                    continue;
+                }
                 String oldPass = dbConnection.getPassword();
                 if (oldPass != null) {
                     dbConnection.setPassword(PasswordMigrationUtil.getEncryptPasswordIfNeed(oldPass));
                     ElementWriterFactory.getInstance().createDataProviderWriter().save(me);
                 }
+                
             }
         }
+
+        // for report datamart part, consider the password
+        List<? extends ModelElement> allElement = RepResourceFileHelper.getInstance().getAllElement();
+        for (ModelElement me : allElement) {
+            if (me instanceof TdReport) {
+                boolean modify = false;
+                TdReport report = (TdReport) me;
+                TaggedValue oldPassword = TaggedValueHelper
+                        .getTaggedValue(TaggedValueHelper.REP_DBINFO_PASSWORD, report.getTaggedValue());
+                String oldPass = oldPassword.getValue();
+                if (oldPass != null) {
+                    String newPassword = PasswordMigrationUtil.getEncryptPasswordIfNeed(oldPass);
+                    TaggedValueHelper.setTaggedValue(report, TaggedValueHelper.REP_DBINFO_PASSWORD, newPassword); // after
+                    modify = true;
+                }
+
+                // for report context part, consider the password
+                EList<ContextType> repContextList = report.getContext();
+                for (ContextType type : repContextList) {
+                    List<ContextParameterType> paramTypes = type.getContextParameter();
+                    if (paramTypes != null) {
+                        for (ContextParameterType param : paramTypes) {
+                            String value = param.getValue();
+                            if (value != null && PasswordEncryptUtil.isPasswordType(param.getType())) {
+                                param.setRawValue(PasswordMigrationUtil.getEncryptPasswordIfNeed(value));
+                                modify = true;
+                            }
+                        }
+                    }
+                }
+                if (modify) {
+                    ElementWriterFactory.getInstance().createReportWriter().save(me);
+                }
+            }
+        }
+
         return true;
     }
 }
